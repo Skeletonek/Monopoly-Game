af02d623deacf9da924fbf42a07f21febb16f50f
Skeletonek <skeleton0199@gmail.com>
2021-04-15 12:51:43 +0200

    A little bit of code clearing

---
+++ Monopoly/Audio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Monopoly
+{
+    public class Audio
+    {
+        public bool active = true;
+        public MediaPlayer music = new MediaPlayer();
+        public string musicfile = @"Resources\music_wait.mp3";
+        public MediaPlayer sfx = new MediaPlayer();
+        public string sfxfile = @"Resources\music_wait.mp3";
+    }
+}
--- Monopoly/MainWindow.xaml.cs
+++ Monopoly/MainWindow.xaml.cs
@@ -33,42 +33,8 @@ namespace Monopoly
         string playboardTheme = "Monopoly Standard";
         public static string currentThemeDir = "Resources";
 
-        public class Audio
-        {
-            public bool active = true;
-            public MediaPlayer music = new MediaPlayer();
-            public string musicfile = @"Resources\music_wait.mp3";
-            public MediaPlayer sfx = new MediaPlayer();
-            public string sfxfile = @"Resources\music_wait.mp3";
-        }
-
         DispatcherTimer wait = new DispatcherTimer();
         DispatcherTimer reload = new DispatcherTimer();
-        static public class Game
-        {
-            static public bool multiplayer = false;
-            static public string[] playername = new string[] { "Gracz 1", "Gracz 2", "Gracz 3", "Gracz 4", "Mr. Nobody" };
-            static public byte[] playerlocation = new byte[] { 0, 0, 0, 0 };
-            static public int[] playercash = new int[] { 1500, 1500, 1500, 1500 };
-            static public byte[] playerRailroadOwned = new byte[] { 0, 0, 0, 0 };
-            static public byte[] playerArrestedTurns = new byte[] { 0, 0, 0, 0 };
-            static public bool[] playerAvailable = new bool[] { false, false, false, false };
-            static public bool[] playerBankrupt = new bool[] { false, false, false, false };
-            static public byte playerBankruptNeededToWin = 0;
-            static public byte clientplayer = 0;
-            static public byte turn = 0;
-            static public byte dice1;
-            static public byte dice2;
-            static public byte selectedField = 0;
-            static public int currentFieldPrice = 0;
-            static public bool currentFieldForSale = false;
-            static public byte[] fieldHouse = new byte[41];
-            static public byte[] fieldOwner = new byte[41];
-            static public byte[] fieldPlayers = new byte[41];
-            static public int taxmoney = 0;
-            static public bool sellmode = false;
-            static public bool dangerzone = false;
-        }
 
         BoardLocations boardLocations = new BoardLocations();
         Audio audio = new Audio();
