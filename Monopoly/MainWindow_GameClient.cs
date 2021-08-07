d0634c064f8b9ce7ff8459b36d1391fca3be7f3c
Skeletonek <skeleton0199@gmail.com>
2021-08-07 20:44:41 +0200

    Fixed wrong buttons on buying houses info MessageBox

--- Monopoly/MainWindow_GameClient.cs
+++ Monopoly/MainWindow_GameClient.cs
@@ -315,7 +315,7 @@ namespace Monopoly
                                 }
                                 if (Game.fieldOwner[currentPlayerLocation] != 4 && Game.fieldOwner[currentPlayerLocation] == Game.fieldOwner[BoardData.fieldSet1[currentPlayerLocation]] && (Game.fieldOwner[currentPlayerLocation] == Game.fieldOwner[BoardData.fieldSet2[currentPlayerLocation]] || BoardData.fieldSet2[currentPlayerLocation] == 0))
                                 {
-                                    MessageBox.Show("Od teraz możesz kupować domy w tej dzielnicy! Aby kupić, kliknij na dane pole lewym przyciskiem myszy przed zakończeniem tury", "Monopoly", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                                    MessageBox.Show("Od teraz możesz kupować domy w tej dzielnicy! Aby kupić, kliknij na dane pole lewym przyciskiem myszy przed zakończeniem tury", "Monopoly", MessageBoxButton.OK, MessageBoxImage.Information);
                                 }
                             }
                             break;
